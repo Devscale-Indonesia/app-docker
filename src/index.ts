5dc0d82be4b7f42b3e6fe429ffb192ccafc479aa
Indra Zulfi <me@indrazm.com>
2024-12-16 21:09:48 +0700

    init aapp

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Elysia } from "elysia";
+import { prisma } from "./utils/prisma";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", async () => {
+    return await prisma.note.findMany();
+  })
+  .listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
